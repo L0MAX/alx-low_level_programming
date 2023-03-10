5f35f86fa2729ec537b35e1803178b48d89e4076
L0MAX <ankahdonatus@gmail.com>
2023-03-10 06:57:41 -0800

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints its name, followed by a new line.
  * @argc: argument count
  * @argv: arguments
  *
- * Return 0
+ * return 0
  */
 int main(int argc, char **argv)
 {
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,6 @@
 #define MAIN.H
 
 int _putchar(char c);
+int _atoi(char *s);
 
 #endif
