c12fcd6f4bb127508e4f6e9249c8c355a01830bc
L0MAX <ankahdonatus@gmail.com>
2023-03-10 08:48:56 -0800

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,8 @@
  *
  * return 0
  */
-int main(int argc, char *argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	(void)argc;
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
