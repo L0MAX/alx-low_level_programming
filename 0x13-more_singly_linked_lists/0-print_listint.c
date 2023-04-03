8462b04ee21067dc1acec2dcf640ec753b39c622
L0MAX <ankahdonatus@gmail.com>
2023-04-03 13:46:21 -0700

    checkmate

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,32 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint - a function that prints all elements of a list
- *
- * @h: pointer to first node
+ * print_listint - prints all the elements of a linked list
+ * @h: head of the list
  *
  * Return: the number of nodes
-*/
+ */
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count = 1;
+const listint_t *cursor = h;
+size_t count = 0;
 
-	/* return 0 as no of nodes when h is null*/
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
-	{
-		printf("%d\n", h->n);
-
-		/*go to the next node*/
-		h = h->next;
-		/*count no of nodes*/
-		node_count++;
-	}
-
-	/*print last node*/
-	printf("%d\n", h->n);
-
-	return (node_count);
+while (cursor != NULL)
+{
+printf("%d\n", cursor->n);
+count += 1;
+cursor = cursor->next;
+}
+return (count);
 }
