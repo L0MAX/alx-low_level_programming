4de81ad33ac07eee12b958651dab3421b6cd4c8c
L0MAX <ankahdonatus@gmail.com>
2023-04-03 14:13:26 -0700

    edzebubu

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t edze = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		edze++;
 	}
-	return (count);
+	return (edze);
 }
+
