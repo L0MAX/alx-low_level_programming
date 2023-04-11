5e30caa5aaad468afc530b0140b8408bf57f4841
L0MAX <ankahdonatus@gmail.com>
2023-04-10 20:22:34 -0700

    updated file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,7 +5,7 @@
 /**
  * check97 - checks for the correct number of arguments
  * @argc: number of arguments
- *
+ * #L0MAX
  * Return: void
  */
 void check97(int argc)
@@ -26,15 +26,15 @@ void check97(int argc)
  *
  * Return: void
  */
-void check98(ssize_t check, char *file, int fdes_from, int fdes_toen)
+void check98(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file);
-		if (fdes_from != -1)
-			close(fdes_from);
-		if (fdes_toen != -1)
-			close(fdes_toen);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
 		exit(98);
 	}
 }
@@ -48,15 +48,15 @@ void check98(ssize_t check, char *file, int fdes_from, int fdes_toen)
  *
  * Return: void
  */
-void check99(ssize_t check, char *file, int fdes_from, int fdes_toen)
+void check99(ssize_t check, char *file, int fd_from, int fd_to)
 {
 	if (check == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-		if (fdes_from != -1)
-			close(fdes_from);
-		if (fdes_toen != -1)
-			close(fdes_toen);
+		if (fd_from != -1)
+			close(fd_from);
+		if (fd_to != -1)
+			close(fd_to);
 		exit(99);
 	}
 }
@@ -68,11 +68,11 @@ void check99(ssize_t check, char *file, int fdes_from, int fdes_toen)
  *
  * Return: void
  */
-void check100(int check, int fdes)
+void check100(int check, int fd)
 {
 	if (check == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fdes %d\n", fdes);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -85,30 +85,30 @@ void check100(int check, int fdes)
  */
 int main(int argc, char *argv[])
 {
-	int fdes_from, fdes_toen, close_toen, closeon_from;
-	ssize_t lenr, lenw;
+	int fd_from, fd_to, close_to, close_from;
+	ssize_t lenrs, lenws;
 	char buffer[1024];
 	mode_t file_perm;
 
 	check97(argc);
-	fdes_from = open(argv[1], O_RDONLY);
-	check98((ssize_t)fdes_from, argv[1], -1, -1);
+	fd_from = open(argv[1], O_RDONLY);
+	check98((ssize_t)fd_from, argv[1], -1, -1);
 	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	fdes_toen = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
-	check99((ssize_t)fdes_toen, argv[2], fdes_from, -1);
-	lenr = 1024;
-	while (lenr == 1024)
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
+	check99((ssize_t)fd_to, argv[2], fd_from, -1);
+	lenrs = 1024;
+	while (lenrs == 1024)
 	{
-		lenr = read(fdes_from, buffer, 1024);
-		check98(lenr, argv[1], fdes_from, fdes_toen);
-		lenw = write(fdes_toen, buffer, lenr);
-		if (lenw != lenr)
-			lenw = -1;
-		check99(lenw, argv[2], fdes_from, fdes_toen);
+		lenrs = read(fd_from, buffer, 1024);
+		check98(lenrs, argv[1], fd_from, fd_to);
+		lenws = write(fd_to, buffer, lenrs);
+		if (lenws != lenrs)
+			lenws = -1;
+		check99(lenws, argv[2], fd_from, fd_to);
 	}
-	close_toen = close(fdes_toen);
-	closeon_from = close(fdes_from);
-	check100(close_toen, fdes_toen);
-	check100(closeon_from, fdes_from);
+	close_to = close(fd_to);
+	close_from = close(fd_from);
+	check100(close_to, fd_to);
+	check100(close_from, fd_from);
 	return (0);
 }
